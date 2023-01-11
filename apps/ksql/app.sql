b8d55f86f602f82d27a82f7ee684d9ca57715186
pietrolatorre <pietro.latorre@quantyca.it>
2023-01-11 12:26:02 +0100

    updated ksql script

--- apps/ksql/app.sql
+++ apps/ksql/app.sql
@@ -1,30 +1,8 @@
-CREATE STREAM tripsFromSystemA
-  (tripId BIGINT,
-   tripCategory VARCHAR,
-   tripStatus VARCHAR,
-   tripStartTs TIMESTAMP)
-  WITH (KAFKA_TOPIC='tripsFromSystemA',
-        VALUE_FORMAT='AVRO');
-
-CREATE STREAM tripsFromSystemB
-  (id BIGINT,
-   "status" VARCHAR,
-   startTs TIMESTAMP)
-  WITH (KAFKA_TOPIC='tripsFromSystemB',
-        VALUE_FORMAT='AVRO');
-
-CREATE STREAM trips
-  (tripId BIGINT,
-   tripCategory VARCHAR,
-   tripStatus VARCHAR,
-   tripStartTs TIMESTAMP)
-   WITH (KAFKA_TOPIC='trips',
-        VALUE_FORMAT='AVRO');
-
-INSERT INTO trips 
-SELECT tripId, tripCategory, tripStatus, tripStartTs, currentTs
-FROM tripsFromSystemA EMIT CHANGES;
-
-INSERT INTO trips 
-SELECT id as tripId, 'B' as tripCategory, "status" as tripStatus, startTs as tripStartTs, currentTs
-FROM tripsFromSystemB EMIT CHANGES;
+CREATE TABLE tripCurrentSnapshot AS
+SELECT tripId,
+       LATEST_BY_OFFSET(tripStatus) as tripCurrentStatus,
+       LATEST_BY_OFFSET(vectorPositionLat) as vectorCurrentPositionLat,
+       LATEST_BY_OFFSET(vectorPositionLong) as vectorCurrentPositionLong,
+       LATEST_BY_OFFSET(eventTimestamp) as eventTimestamp
+FROM TRIPEVENT
+GROUP BY tripId;
